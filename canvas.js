7936453ce60cb930ebaa22a43427f72495e0ad95
ysulaeman2010 <ysulaeman2010@gmail.com>
2021-04-14 04:05:07 +0700

    first update

---
+++ canvas.js
@@ -0,0 +1,23 @@
+console.log("starting");
+
+const canvas = document.querySelector("canvas");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const c = canvas.getContext("2d");
+
+let pos = {
+  x: undefined,
+  y: undefined,
+};
+
+window.addEventListener("click", function (event) {
+  pos.x = event.x;
+  pos.y = event.y;
+  console.log(`x ${pos.x} y ${pos.y}`);
+  c.beginPath();
+  c.arc(pos.x, pos.y, 30, 0, Math.PI*2,false);
+  c.strokeStyle = 'black';
+  c.stroke();
+});
